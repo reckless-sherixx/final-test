b97c46deab64098893534152dde2437eab0c7c46
blizdia1 <blizdiawork@gmail.com>
2024-11-11 21:39:16 +0530

    Update authapi.js

--- Frontend/src/redux/features/auth/authapi.js
+++ Frontend/src/redux/features/auth/authapi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/auth",
+        baseUrl: "https://school-web-test.onrender.com/api/auth",
         credentials: "include"
     }),
     endpoints: (builder) => ({
