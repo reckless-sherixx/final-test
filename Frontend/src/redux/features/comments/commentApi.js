dcc3199234561a1e3e1c31f54de71579d95e1507
blizdia1 <blizdiawork@gmail.com>
2024-11-11 21:39:51 +0530

    Update commentApi.js

--- Frontend/src/redux/features/comments/commentApi.js
+++ Frontend/src/redux/features/comments/commentApi.js
@@ -3,7 +3,7 @@ import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const commentApi = createApi({
     reducerPath: 'commentApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5000/api/comments',
+        baseUrl: 'https://school-web-test.onrender.com/api/comments',
         credentials: "include",
     }),
     tagTypes: ['Comments'],
