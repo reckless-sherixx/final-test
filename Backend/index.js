4a05468bbeae2f7fa1cc3d8f48e420d877517c82
blizdia1 <blizdiawork@gmail.com>
2024-11-11 22:03:36 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://school-web-test-front.onrender.com/',
     credentials: true, 
 }));
 
