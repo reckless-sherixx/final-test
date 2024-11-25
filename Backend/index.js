e506fb4bd0f4ba2d0a9277f96ab5033257ca7b4b
blizdia1 <blizdiawork@gmail.com>
2024-11-25 18:55:43 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,7 @@ const mongoose =  require('mongoose');
 const app =  express();
 const cors  = require('cors');
 require ('dotenv').config()
-const port = process.env.PORT  || 5000;
+// const port = process.env.PORT  || 5000;
 
 // parse options
 app.use(express.json());
