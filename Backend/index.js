727c039f1bbb09123d65eb58e7470837c3ef689f
blizdia1 <blizdiawork@gmail.com>
2024-11-26 15:19:19 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -38,9 +38,9 @@ app.get('/', (req, res) => {
     res.send('Hello Worldd!');
 });
 
-app.listen (port, () => {
-    console.log(`Server is running on port ${port}`)
-});
+// app.listen (port, () => {
+//     console.log(`Server is running on port ${port}`)
+// });
 
 
 // user : blizdia69
